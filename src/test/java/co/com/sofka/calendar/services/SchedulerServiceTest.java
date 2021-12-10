440055f4fbbefa4d8a884389691d6a81d970fe35
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-12-10 15:23:24 -0500

    realizado test de dos formas

--- src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
+++ src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
@@ -31,10 +31,9 @@ class SchedulerServiceTest {
     @Mock
     ProgramRepository repository;
 
-
     @Test
 
-        //TODO: modificar el test para que el act sea reactivo, usando stepverifier
+    // TODO: modificar el test para que el act sea reactivo, usando stepverifier
 
     void generateCalendar() {
         var programId = "xxxx";
@@ -43,17 +42,47 @@ class SchedulerServiceTest {
         Program program = getProgramDummy();
 
         Mockito.when(repository.findById(programId)).thenReturn(Mono.just(program));
-        
-        //TODO: hacer una subscripción de el servicio reactivo
+
+        // TODO: hacer una subscripción de el servicio reactivo
 
         Flux<ProgramDate> response = schedulerService.generateCalendar(programId, startDate);
-        
+
         response.subscribe();
 
-        StepVerifier.create(response).expectNextCount(13).verifyComplete();
-        
         // Assertions.assertEquals(13, response.single());//TODO: hacer de otro modo
         // Assertions.assertEquals(getSnapResult(), new Gson().toJson(response));//TODO: hacer de otro modo
+
+        StepVerifier.create(response)
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-03")
+                            && programDate.getCategoryName().equals("Principios");
+                })
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-04")
+                            && programDate.getCategoryName().equals("Bases");
+                })
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-05")
+                            && programDate.getCategoryName().equals("Bases");
+                })
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-06")
+                            && programDate.getCategoryName().equals("Fundamentos");
+                })
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-07")
+                            && programDate.getCategoryName().equals("Fundamentos");
+                })
+                .expectNextMatches(programDate -> {
+                    return programDate.getDate().toString().equals("2022-01-10")
+                            && programDate.getCategoryName().equals("Fundamentos");
+                })
+                .verifyComplete();
+
+        StepVerifier.create(response)
+                .expectNextCount(6)
+                .verifyComplete();
+
         Mockito.verify(repository).findById(programId);
     }
 
@@ -64,36 +93,36 @@ class SchedulerServiceTest {
 
         Mockito.when(repository.findById(programId)).thenReturn(Mono.empty());
 
-        //TODO: hacer de otro modo
+        // TODO: hacer de otro modo
         var exception = Assertions.assertThrows(RuntimeException.class, () -> {
-            schedulerService.generateCalendar(programId, startDate);//TODO: hacer una subscripción de el servicio reactivo
+            schedulerService.generateCalendar(programId, startDate);// TODO: hacer una subscripción de el servicio
+                                                                    // reactivo
 
         });
-        Assertions.assertEquals("El programa academnico no existe", exception.getMessage());//TODO: hacer de otro modo
+        Assertions.assertEquals("El programa academnico no existe", exception.getMessage());// TODO: hacer de otro modo
         Mockito.verify(repository).findById(programId);
 
     }
 
-    //no tocar
+    // no tocar
     private Program getProgramDummy() {
         var program = new Program();
         program.setId("xxxx");
         program.setName("Programa 2022");
         program.setCourses(new ArrayList<>());
         var timesForCourse1 = new ArrayList<Time>();
-        timesForCourse1.add(new Time("1", 2, "Principios", List.of()));
+        timesForCourse1.add(new Time("1", 1, "Principios", List.of()));
         timesForCourse1.add(new Time("2", 2, "Bases", List.of()));
-        timesForCourse1.add(new Time("3", 4, "Fundamentos", List.of()));
-        timesForCourse1.add(new Time("3", 5, "Fundamentos avazandos", List.of()));
+        timesForCourse1.add(new Time("3", 3, "Fundamentos", List.of()));
+        // timesForCourse1.add(new Time("3", 5, "Fundamentos avazandos", List.of()));
 
         program.getCourses().add(new CourseTime("xxx-z", "Introducción", timesForCourse1));
         return program;
     }
 
-    //no tocar
+    // no tocar
     private String getSnapResult() {
         return "[{\"categoryName\":\"Principios\",\"date\":{\"year\":2022,\"month\":1,\"day\":3}},{\"categoryName\":\"Principios\",\"date\":{\"year\":2022,\"month\":1,\"day\":4}},{\"categoryName\":\"Bases\",\"date\":{\"year\":2022,\"month\":1,\"day\":5}},{\"categoryName\":\"Bases\",\"date\":{\"year\":2022,\"month\":1,\"day\":6}},{\"categoryName\":\"Fundamentos\",\"date\":{\"year\":2022,\"month\":1,\"day\":7}},{\"categoryName\":\"Fundamentos\",\"date\":{\"year\":2022,\"month\":1,\"day\":10}},{\"categoryName\":\"Fundamentos\",\"date\":{\"year\":2022,\"month\":1,\"day\":11}},{\"categoryName\":\"Fundamentos\",\"date\":{\"year\":2022,\"month\":1,\"day\":12}},{\"categoryName\":\"Fundamentos avazandos\",\"date\":{\"year\":2022,\"month\":1,\"day\":13}},{\"categoryName\":\"Fundamentos avazandos\",\"date\":{\"year\":2022,\"month\":1,\"day\":14}},{\"categoryName\":\"Fundamentos avazandos\",\"date\":{\"year\":2022,\"month\":1,\"day\":17}},{\"categoryName\":\"Fundamentos avazandos\",\"date\":{\"year\":2022,\"month\":1,\"day\":18}},{\"categoryName\":\"Fundamentos avazandos\",\"date\":{\"year\":2022,\"month\":1,\"day\":19}}]";
     }
-
 
 }
